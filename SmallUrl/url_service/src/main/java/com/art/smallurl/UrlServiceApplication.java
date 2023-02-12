c59563b93dbebd90bb821bc8a1fb299e74cd24c8
tejavenkatlanka <tvlanka9@gmail.com>
2023-02-13 01:00:02 +0530

    Url service setup lombok

---
+++ SmallUrl/url_service/src/main/java/com/art/smallurl/UrlServiceApplication.java
@@ -0,0 +1,12 @@
+package com.art.smallurl;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Slf4j
+public class UrlServiceApplication {
+    public static void main(String[] args) {
+        log.info("UrlServiceApplication started");
+    }
+}
