5c5b9837b2dfc45bcf3ad6693def8721a656e5f0
tejavenkat lanka <tvlanka9@gmail.com>
2023-02-14 23:26:27 +0530

    New controller for url service

---
+++ SmallUrl/url_service/src/main/java/com/art/smallurl/controller/SmallUrlController.java
@@ -0,0 +1,10 @@
+package com.art.smallurl.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController("/api/v1/small-url")
+public class SmallUrlController {
+
+
+}
